493c94136693b9b58dac5dfac182bef10964f24d
soohoonkim <soohoon.kim@hotmail.com>
2021-08-27 10:36:51 +0900

    Created a form for users to submit a new post in model, view, and tempates

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, FormView
+from .forms import PostForm
 from .models import Post
 
 # Create your views here.
@@ -15,3 +16,15 @@ class PostDetailView(DetailView):
     template_name = 'detail.html'
     model = Post
     
+class AddPostView(FormView):
+    template_name = 'new_post.html'
+    form_class = PostForm
+    success_url = '/' #add a separate html later with home link?
+
+    def form_valid(self, form):
+        #creating a new post on the home.html
+        new_object = Post.objects.create(
+            text=form.cleaned_data['text'],
+            image=form.cleaned_data['image']
+        )
+        return super().form_valid(form)
