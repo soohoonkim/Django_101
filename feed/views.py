6fdfd28a27334d23ab1ac541dfeb025fa75bb7a5
soohoonkim <soohoon.kim@hotmail.com>
2021-08-29 00:35:42 +0900

    minor change test

--- feed/views.py
+++ feed/views.py
@@ -9,7 +9,7 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['Welcome'] = "Hello and Welcome! This is a Website Made with Django :)"
+        context['Welcome'] = "Hello and Welcome! This is a Website Made with Django"
         context['posts'] = Post.objects.all().order_by('-id')
         return context
 
